ceba7ae8feb90ea6d4b709823e6d849bcdc27a34
Ginner Zapata <gzapata@vizzn.ca>
2023-05-29 20:39:49 -0300

    handle post

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const fs = require('fs')
 const express = require('express')
 
 const app = express()
+//middleware to use json
+app.use(express.json())
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -13,7 +15,24 @@ app.get('/api/v1/tours', (req, res) => {
     .json({ status: 'success', results: tours.length, data: { tours } })
 })
 
-app.post()
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1
+  const newTour = Object.assign({ id: newId }, req.body)
+
+  tours.push(newTour)
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).send({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      })
+    }
+  )
+})
 
 const port = 3000
 app.listen(port, () => {
