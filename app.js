859317ac256a44abaa2bf7334e53921aa5f2dc14
Ginner Zapata <gzapata@vizzn.ca>
2023-05-29 20:08:56 -0300

    basic of express

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
-const express = require("express");
-const app = express();
+const express = require('express')
 
-const port = 3000;
+const app = express()
 
-app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello from the server side" });
-});
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'Natours' })
+})
 
-app.post("/", (req, res) => {
-  res.send("You posted!");
-});
-
+const port = 3000
 app.listen(port, () => {
-  console.log(`running on port ${port}`);
-});
+  console.log(`App running on port ${port}`)
+})
